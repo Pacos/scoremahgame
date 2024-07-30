b1db969ad3f8494a6720b616a0ea9d6eea488448
Pascal Halter <pascos@gmail.com>
2024-07-30 15:50:55 +0200

    first simple layout tests

--- src/scoremahgame/app.py
+++ src/scoremahgame/app.py
@@ -9,13 +9,22 @@ from toga.style.pack import COLUMN, ROW
 
 class ScoreMahGame(toga.App):
     def startup(self):
-        """Construct and show the Toga application.
+        main_box = toga.Box(style=Pack(direction=COLUMN))
 
-        Usually, you would add your application to a main content box.
-        We then create a main window (with a name matching the app), and
-        show the main window.
-        """
-        main_box = toga.Box()
+        def new_game_handler(widget):
+            print("new_game_button pressed")
+
+        def continue_game_handler(widget):
+            print("continue_game_button pressed")
+
+        title_label = toga.Label("Score Mah Game", style=Pack(color="#45d18b", text_align="center", font_size="32", padding=10))
+
+        new_game_button = toga.Button("New Game", on_press=new_game_handler, style=Pack(padding=10))
+        continue_game_button = toga.Button("Continue", on_press=continue_game_handler, enabled=False, style=Pack(padding=10))
+
+        main_box.add(title_label)
+        main_box.add(new_game_button)
+        main_box.add(continue_game_button)
 
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
